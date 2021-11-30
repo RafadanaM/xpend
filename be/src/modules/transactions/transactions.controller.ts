7b0a448d5f20daf1ff8716178e7960521fb4df13
Yafi Ahsan Hakim <hakimy500@gmail.com>
2021-11-30 11:25:30 +0700

    add transaction module

---
+++ be/src/modules/transactions/transactions.controller.ts
@@ -0,0 +1,41 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import { RequestTypes } from '../../enums/request.enum';
+import Controller from '../../interfaces/controller.interface';
+import validationMiddleware from '../../middlewares/validation.middleware';
+import createTransactionDto from './transactions.dto';
+import TransactionsService from './transactions.service';
+
+class TransactionsController implements Controller {
+  public path: string = '/transactions';
+  public router: Router = Router();
+  public transactionsService: TransactionsService;
+
+  constructor() {
+    this.transactionsService = new TransactionsService();
+    this.initRoutes();
+  }
+
+  private initRoutes() {
+    this.router.get('', this.getTransactions);
+    this.router.post('', validationMiddleware(createTransactionDto, RequestTypes.BODY), this.createTransaction);
+  }
+
+  private createTransaction = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const transactionData: createTransactionDto = req.body;
+      res.send(await this.transactionsService.createTransaction(transactionData));
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  private getTransactions = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      res.send(await this.transactionsService.getTransactions());
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default TransactionsController;
