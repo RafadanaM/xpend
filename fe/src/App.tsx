7b0a08c4d1aed9f36cecf4d39764555e38bde493
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-12-01 17:50:02 +0700

    feat: add basic routes

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Landing } from "./pages/Landing";
+import { NotFound } from "./pages/NotFound";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
