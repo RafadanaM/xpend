609636a5424cf91b0bb4a230a96321ab98cba033
Yafi Ahsan Hakim <hakimy500@gmail.com>
2021-12-02 10:42:21 +0700

    create loginForm component

--- fe/src/pages/Landing.tsx
+++ fe/src/pages/Landing.tsx
@@ -1,3 +1,10 @@
+import LoginForm from "../components/LoginForm"
+
 export const Landing = () => {
-  return <div className="">Landing page</div>;
+  return (
+    <div className="grid grid-cols-2 m-auto">
+      <div>Landing page</div>
+      <div className=""><LoginForm/></div>
+    </div>
+  );
 };
