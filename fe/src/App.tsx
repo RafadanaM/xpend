c48f6c7ac140ef050690e2a7d54147d0d95732a9
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-12-05 21:19:48 +0700

    chore: re-arrange files

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Footer from "./components/Layouts/Footer";
+import Navbar from "./components/Layouts/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import { Home } from "./pages/Home";
 import { Landing } from "./pages/Landing";
