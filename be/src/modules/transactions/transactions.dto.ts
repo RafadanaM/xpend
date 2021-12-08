2cae55dcdbe8b5b6c4f577704461934832314a85
Yafi Ahsan Hakim <hakimy500@gmail.com>
2021-12-08 09:41:32 +0700

    update transaction table

--- be/src/modules/transactions/transactions.dto.ts
+++ be/src/modules/transactions/transactions.dto.ts
@@ -1,10 +1,16 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsDateString, IsNumber, IsString } from 'class-validator';
 
 class createTransactionDto {
+  @IsString()
+  public title: string;
+
   @IsString()
   public description: string;
 
   @IsNumber()
   public amount: number;
+
+  @IsDateString()
+  public date: Date;
 }
 export default createTransactionDto;
