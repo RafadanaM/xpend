ae0205ca4784c913f82e580ef328ff0c02c16b8c
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-12-08 10:04:44 +0700

    feat: add edit modal

---
+++ fe/src/components/Modals/TransactionModal.tsx
@@ -0,0 +1,197 @@
+import { FormEvent, useEffect, useRef, useState } from "react";
+import { TransactionService } from "../../api/services/TransactionService";
+import { ReactComponent as CloseIcon } from "../../assets/close.svg";
+import Transaction from "../../interfaces/transaction.interface";
+import {
+  formatDate,
+  formatToInput,
+  transactionDateFormat,
+} from "../../utils/formatDate";
+import { transactionInputs } from "../../utils/formInputs";
+import useOutsideAlerter from "../../utils/useOutsideAlerter";
+import FormInput from "../Forms/FormInput";
+
+type TransactionFormType = {
+  title: string;
+  description: string;
+  amount: number;
+  date: string;
+};
+
+interface AddTransactionModalI {
+  open: boolean;
+  changeOpen: Function;
+  setTransactions: Function;
+  transaction?: Transaction;
+  setSelectedTransaction: Function;
+}
+
+export const TransactionModal = ({
+  open,
+  changeOpen,
+  setTransactions,
+  transaction,
+  setSelectedTransaction,
+}: AddTransactionModalI) => {
+  const modalRef = useRef(null);
+  useOutsideAlerter(modalRef, () => {
+    handleClose();
+  });
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [focused, setFocused] = useState<boolean[]>([
+    false,
+    false,
+    false,
+    false,
+  ]);
+  const [values, setValues] = useState<TransactionFormType>({
+    title: "",
+    description: "",
+    amount: 0,
+    date: "",
+  });
+
+  useEffect(() => {
+    setValues({
+      title: transaction ? "Title" : "",
+      description: transaction ? transaction.description : "",
+      amount: transaction ? transaction.amount : 0,
+      date: transaction ? formatToInput(transaction.created) : "",
+    });
+  }, [isEdit, transaction]);
+
+  const handleClose = () => {
+    changeOpen(false);
+    setSelectedTransaction(false);
+    setIsEdit(false);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (isEdit) {
+      console.log("SAVE EDIT");
+    } else {
+      TransactionService.createTransaction(
+        values.title,
+        +values.amount,
+        values.description,
+        values.date
+      )
+        .then(async (_) => {
+          const { data } = await TransactionService.getTransactions();
+          setTransactions(data);
+          changeOpen(false);
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    }
+  };
+
+  const handleFocus = (index: number) => {
+    focused[index] = true;
+    setFocused([...focused]);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  let valueKeys = Object.keys(values) as (keyof TransactionFormType)[];
+  return open ? (
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-60 z-20 flex justify-center items-center">
+      <div
+        className="bg-white md:w-11/12 md:max-w-lg w-full h-full md:h-auto z-50 py-2 flex flex-col"
+        ref={modalRef}
+      >
+        {!transaction || isEdit ? (
+          <>
+            <div className="py-2 w-full border-b-2 px-8 font-semibold text-xl border-accent-grey flex justify-between">
+              <span>{isEdit ? "Edit Transaction" : "New Transaction"}</span>
+              <CloseIcon
+                className="-mx-4 w-4 h-4 cursor-pointer"
+                onClick={handleClose}
+              />
+            </div>
+            <div className="px-8 py-4">
+              <form onSubmit={handleSubmit}>
+                {transactionInputs.map((input, index) => {
+                  return (
+                    <FormInput
+                      key={input.id}
+                      {...input}
+                      value={values[valueKeys[index]]}
+                      onChange={onChange}
+                      onBlur={() => handleFocus(index)}
+                      focused={focused[index]}
+                      labelStyle="font-medium"
+                    />
+                  );
+                })}
+
+                <div className="flex items-center gap-x-4 justify-between mt-8">
+                  {transaction && (
+                    <button
+                      className="bg-red-600 px-2 py-1 font-medium flex-1 text-white hover:bg-opacity-75"
+                      onClick={() => setIsEdit(false)}
+                      type="button"
+                    >
+                      Cancel
+                    </button>
+                  )}
+
+                  <button
+                    className="flex-1 bg-accent-orange hover:bg-opacity-75 text-white font-medium py-1 px-4 focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    {isEdit ? "Save" : "Create"}
+                  </button>
+                </div>
+              </form>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="py-2 w-full border-b-2 px-8 font-semibold text-xl border-accent-grey flex justify-between">
+              <span>Transaction Detail</span>
+              <CloseIcon
+                className="-mx-4 w-4 h-4 cursor-pointer"
+                onClick={handleClose}
+              />
+            </div>
+            <div className="px-8 py-4 flex flex-col">
+              <span className="text-lg font-medium">Title</span>
+              <span className="text-gray-700 break-words">
+                transaction title
+              </span>
+              <span className="text-lg font-medium mt-5">Description</span>
+              <span className="text-gray-700 break-words">
+                {transaction.description}
+              </span>
+              <span className="text-lg font-medium mt-5">Amount</span>
+              <span className="text-gray-700">{`Rp. ${transaction.amount}`}</span>
+              <span className="text-lg font-medium mt-5">Date</span>
+              <span className="text-gray-700">
+                {formatDate(transaction.created, transactionDateFormat)}
+              </span>
+              <div className="flex w-full mt-10 gap-x-4">
+                <button
+                  className="bg-red-600 px-2 py-1 flex-1 text-white hover:bg-opacity-75"
+                  onClick={() => setIsEdit(true)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="bg-accent-orange flex-1 px-2 py-1 text-white hover:bg-opacity-75"
+                  onClick={() => setIsEdit(true)}
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  ) : null;
+};
--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -1,30 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { TransactionService } from "../api/services/TransactionService";
 import { SummaryCard } from "../components/Cards/SummaryCard";
 import { TaskCard } from "../components/Cards/TaskCard";
 import { Filter } from "../components/Filter";
-import { AddTransactionModal } from "../components/Modals/AddTransactionModal";
+import { TransactionModal } from "../components/Modals/TransactionModal";
 import { Table } from "../components/Table";
+import Transaction from "../interfaces/transaction.interface";
 
 export const Home = () => {
   const [openTransactionModal, setOpenTransactionModal] = useState(false);
+  const [selectedTransaction, setSelectedTransaction] = useState<
+    Transaction | undefined
+  >();
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const handleOpenDetailTransactionModal = (transaction: Transaction) => {
+    console.log(transaction);
+    setSelectedTransaction(transaction);
+    setOpenTransactionModal(true);
+  };
+
+  useEffect(() => {
+    TransactionService.getTransactions()
+      .then(({ data }) => {
+        console.log(data);
+        setTransactions(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <>
-      <AddTransactionModal
+      <TransactionModal
         open={openTransactionModal}
         changeOpen={setOpenTransactionModal}
+        setTransactions={setTransactions}
+        transaction={selectedTransaction}
+        setSelectedTransaction={setSelectedTransaction}
+        // type="create"
       />
+      {/* <TransactionModal
+        open={openDetailTransactionModal}
+        changeOpen={setOpenDetailTransactionModal}
+        setTransactions={setTransactions}
+        transaction={selectedTransaction}
+        type="detail"
+      /> */}
       <div className="grid grid-cols-2 md:px-0 md:py-8 gap-x-10 md:gap-y-2 bg-secondary md:bg-white">
         <SummaryCard />
         <TaskCard />
         <Filter />
 
         <button
-          className="px-2 col-start-2 py-1.5 bg-accent-orange text-white text-xs rounded ml-auto my-auto mr-2 md:mr-0 md:mt-auto"
+          className="h-8 w-28 col-start-2 bg-accent-orange text-white text-xs rounded ml-auto my-auto mr-1 md:mr-0 md:my-0 md:mt-auto"
           onClick={() => setOpenTransactionModal(true)}
         >
           Add Transaction
         </button>
-        <Table />
+        <Table
+          transactions={transactions}
+          onRowClick={handleOpenDetailTransactionModal}
+        />
       </div>
     </>
   );
