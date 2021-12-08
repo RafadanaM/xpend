ae84d383664c537ed2fda27b69ba77c3da2b8a6d
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-12-08 10:43:13 +0700

    change to show created to date

--- fe/src/components/Forms/FormInput.tsx
+++ fe/src/components/Forms/FormInput.tsx
@@ -30,44 +30,37 @@ const FormInput = ({
   return (
     <div className="mb-4">
       <label className={`block text-sm mb-2 ${labelStyle}`}>{label}</label>
-      <input
-        required
-        disabled={disabled}
-        className={`appearance-none border rounded w-full py-2 px-3 leading-tight ${
-          disabled
-            ? "border-white text-black shadow-none"
-            : "border-secondary text-gray-700 shadow focus:outline-none focus:shadow-outline"
-        }`}
-        id={id}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        rows={4}
-        onChange={
-          onChange as unknown as React.ChangeEventHandler<HTMLTextAreaElement>
-        }
-        onBlur={
-          onBlur as unknown as React.ChangeEventHandler<HTMLTextAreaElement>
-        }
-      />
+      {type === "textarea" ? (
+        <textarea
+          required
+          className="appearance-none border rounded w-full py-2 px-3 leading-tight border-secondary text-gray-700 shadow focus:outline-none focus:shadow-outline"
+          id={id}
+          name={name}
+          placeholder={placeholder}
+          value={value}
+          rows={4}
+          onChange={
+            onChange as unknown as React.ChangeEventHandler<HTMLTextAreaElement>
+          }
+          onBlur={
+            onBlur as unknown as React.ChangeEventHandler<HTMLTextAreaElement>
+          }
+        />
       ) : (
-      <input
-        required
-        disabled={disabled}
-        className={`appearance-none border rounded w-full py-2 px-3 leading-tight ${
-          disabled
-            ? "border-white text-black shadow-none"
-            : "border-secondary text-gray-700 shadow focus:outline-none focus:shadow-outline"
-        }`}
-        id={id}
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        pattern={pattern}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
+        <input
+          required
+          className="appearance-none border rounded w-full py-2 px-3 leading-tight border-secondary text-gray-700 shadow focus:outline-none focus:shadow-outline"
+          id={id}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          pattern={pattern}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      )}
+
       {focused && errorMessage ? (
         <span className="text-red-500 text-xs italic hidden my-1">
           {errorMessage}
--- fe/src/interfaces/transaction.interface.ts
+++ fe/src/interfaces/transaction.interface.ts
@@ -2,8 +2,10 @@ import User from "./user.interface";
 
 interface Transaction {
   id: number;
+  title: string;
   description: string;
   amount: number;
+  date: string;
   created: string;
   updated: string;
   user: User;
