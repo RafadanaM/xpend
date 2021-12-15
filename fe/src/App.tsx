71e42332056acb1057c563fe36a0443b030a515b
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-12-15 11:31:47 +0700

    feat: add icons

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Layouts/Footer";
 import Navbar from "./components/Layouts/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
-import CustomRouter from "./CustomRouter";
-import history from "./history";
 import { Home } from "./pages/Home";
 import { Landing } from "./pages/Landing";
 import { NotFound } from "./pages/NotFound";
@@ -12,7 +10,7 @@ import { AuthProvider } from "./utils/useAuth";
 
 function App() {
   return (
-    <CustomRouter history={history}>
+    <BrowserRouter>
       <AuthProvider>
         <Navbar />
         <Routes>
@@ -25,7 +23,7 @@ function App() {
         </Routes>
         <Footer />
       </AuthProvider>
-    </CustomRouter>
+    </BrowserRouter>
   );
 }
 
--- fe/src/api/ApiClient.ts
+++ fe/src/api/ApiClient.ts
@@ -1,19 +1,8 @@
 import axios, { AxiosInstance } from "axios";
-import history from "../history";
 const apiClient: AxiosInstance = axios.create({
   baseURL: "/api",
   responseType: "json",
   withCredentials: true,
-});
-
-apiClient.interceptors.response.use(undefined, (error) => {
-  const status = error?.response?.status;
-
-  if (status === 401) {
-    history.push("/");
-  }
-
-  return Promise.reject(error);
 });
 
 export default apiClient;
